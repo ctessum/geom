120ef316701bec3ea29a764bb671f59cc4ed36a3
Patrick Higgins <patrick.allen.higgins@gmail.com>
2012-05-16 17:13:37 -0600

    Ran gofmt.

--- rtree_test.go
+++ rtree_test.go
@@ -2,9 +2,9 @@ package rtreego
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
 	"testing"
-	"math/rand"
 )
 
 func (r *Rect) Bounds() *Rect {
@@ -63,7 +63,7 @@ func verify(t *testing.T, n *node) {
 		return
 	}
 	for _, e := range n.entries {
-		if e.child.level != n.level - 1 {
+		if e.child.level != n.level-1 {
 			t.Errorf("failed to preserve level order")
 		}
 		if e.child.parent != n {
@@ -88,7 +88,7 @@ var chooseLeafNodeTests = []struct {
 	bb0, bb1, bb2 *Rect // leaf bounding boxes
 	exp           int   // expected chosen leaf
 	desc          string
-	level int
+	level         int
 }{
 	{
 		mustRect(Point{1, 1, 1}, []float64{1, 1, 1}),
@@ -554,7 +554,7 @@ func TestCondenseTreeEliminate(t *testing.T) {
 	for _, thing := range things {
 		rt.Insert(thing)
 	}
-	
+
 	// delete entry 2 from parent entries
 	parent := rt.root.entries[0].child.entries[1].child
 	parent.entries = append(parent.entries[:2], parent.entries[3:]...)
@@ -564,7 +564,7 @@ func TestCondenseTreeEliminate(t *testing.T) {
 	for obj := range items(rt.root) {
 		retrieved = append(retrieved, obj)
 	}
-	
+
 	if len(retrieved) != len(things)-1 {
 		t.Errorf("condenseTree failed to reinsert upstream elements")
 	}
@@ -589,7 +589,7 @@ func TestChooseNodeNonLeaf(t *testing.T) {
 	for _, thing := range things {
 		rt.Insert(thing)
 	}
-	
+
 	obj := mustRect(Point{0, 10}, []float64{1, 2})
 	e := entry{obj, nil, obj}
 	n := rt.chooseNode(rt.root, e, 2)
@@ -667,15 +667,15 @@ func TestDelete(t *testing.T) {
 		things2 = append(things2, things[i])
 		things = append(things[:i], things[i+1:]...)
 	}
-	
+
 	for i, thing := range things2 {
 		ok := rt.Delete(thing)
 		if !ok {
 			t.Errorf("Thing %v was not found in tree during deletion", thing)
 			return
 		}
-		
-		if rt.Size() != len(things2) - i - 1 {
+
+		if rt.Size() != len(things2)-i-1 {
 			t.Errorf("Delete failed to remove %v", thing)
 			return
 		}
@@ -773,12 +773,12 @@ func TestNearestNeighbor(t *testing.T) {
 	for _, thing := range things {
 		rt.Insert(thing)
 	}
-	
+
 	obj1 := rt.NearestNeighbor(Point{0.5, 0.5})
 	obj2 := rt.NearestNeighbor(Point{1.5, 4.5})
 	obj3 := rt.NearestNeighbor(Point{5, 2.5})
 	obj4 := rt.NearestNeighbor(Point{3.5, 2.5})
-	
+
 	if obj1 != things[0] || obj2 != things[1] || obj3 != things[2] || obj4 != things[2] {
 		t.Errorf("NearestNeighbor failed")
 	}
@@ -797,7 +797,7 @@ func TestNearestNeighbors(t *testing.T) {
 	for _, thing := range things {
 		rt.Insert(thing)
 	}
-	
+
 	objs := rt.NearestNeighbors(3, Point{0.5, 0.5})
 	if objs[0] != things[0] || objs[1] != things[2] || objs[2] != things[5] {
 		t.Errorf("NearestNeighbors failed")
