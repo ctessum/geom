a0817c09c7c648ac959fe6e4a054b81350cef220
Tom Payne <twpayne@gmail.com>
2013-10-05 21:14:57 +0200

    Merge WKB functionality into a single file

--- wkb/wkb.go
+++ wkb/wkb.go
@@ -2,6 +2,7 @@ package geom
 
 import (
 	"encoding/binary"
+	"fmt"
 	"io"
 )
 
@@ -110,4 +111,389 @@ func (PolygonM) wkbGeometryType() uint32 {
 
 func (PolygonZM) wkbGeometryType() uint32 {
 	return wkbPolygonZM
+}
+
+
+type wkbReader func(io.Reader, binary.ByteOrder) (Geom, error)
+
+var wkbReaders = map[uint32]wkbReader{
+	wkbPoint:        pointReader,
+	wkbPointZ:       pointZReader,
+	wkbPointM:       pointMReader,
+	wkbPointZM:      pointZMReader,
+	wkbLineString:   lineStringReader,
+	wkbLineStringZ:  lineStringZReader,
+	wkbLineStringM:  lineStringMReader,
+	wkbLineStringZM: lineStringZMReader,
+	wkbPolygon:      polygonReader,
+	wkbPolygonZ:     polygonZReader,
+	wkbPolygonM:     polygonMReader,
+	wkbPolygonZM:    polygonZMReader,
+}
+
+func readLinearRing(r io.Reader, byteOrder binary.ByteOrder) (LinearRing, error) {
+	var numPoints uint32
+	if err := binary.Read(r, byteOrder, &numPoints); err != nil {
+		return nil, err
+	}
+	points := make(LinearRing, numPoints)
+	if err := binary.Read(r, byteOrder, &points); err != nil {
+		return nil, err
+	}
+	return points, nil
+}
+
+func readLinearRingZ(r io.Reader, byteOrder binary.ByteOrder) (LinearRingZ, error) {
+	var numPoints uint32
+	if err := binary.Read(r, byteOrder, &numPoints); err != nil {
+		return nil, err
+	}
+	pointZs := make(LinearRingZ, numPoints)
+	if err := binary.Read(r, byteOrder, &pointZs); err != nil {
+		return nil, err
+	}
+	return pointZs, nil
+}
+
+func readLinearRingM(r io.Reader, byteOrder binary.ByteOrder) (LinearRingM, error) {
+	var numPoints uint32
+	if err := binary.Read(r, byteOrder, &numPoints); err != nil {
+		return nil, err
+	}
+	pointMs := make(LinearRingM, numPoints)
+	if err := binary.Read(r, byteOrder, &pointMs); err != nil {
+		return nil, err
+	}
+	return pointMs, nil
+}
+
+func readLinearRingZM(r io.Reader, byteOrder binary.ByteOrder) (LinearRingZM, error) {
+	var numPoints uint32
+	if err := binary.Read(r, byteOrder, &numPoints); err != nil {
+		return nil, err
+	}
+	pointZMs := make(LinearRingZM, numPoints)
+	if err := binary.Read(r, byteOrder, &pointZMs); err != nil {
+		return nil, err
+	}
+	return pointZMs, nil
+}
+
+func pointReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	point := Point{}
+	if err := binary.Read(r, byteOrder, &point); err != nil {
+		return nil, err
+	}
+	return point, nil
+}
+
+func pointZReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	pointZ := PointZ{}
+	if err := binary.Read(r, byteOrder, &pointZ); err != nil {
+		return nil, err
+	}
+	return pointZ, nil
+}
+
+func pointMReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	pointM := PointM{}
+	if err := binary.Read(r, byteOrder, &pointM); err != nil {
+		return nil, err
+	}
+	return pointM, nil
+}
+
+func pointZMReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	pointZM := PointZM{}
+	if err := binary.Read(r, byteOrder, &pointZM); err != nil {
+		return nil, err
+	}
+	return pointZM, nil
+}
+
+func lineStringReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	points, err := readLinearRing(r, byteOrder)
+	if err != nil {
+		return nil, err
+	}
+	return LineString{points}, nil
+}
+
+func lineStringZReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	pointZs, err := readLinearRingZ(r, byteOrder)
+	if err != nil {
+		return nil, err
+	}
+	return LineStringZ{pointZs}, nil
+}
+
+func lineStringMReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	pointMs, err := readLinearRingM(r, byteOrder)
+	if err != nil {
+		return nil, err
+	}
+	return LineStringM{pointMs}, nil
+}
+
+func lineStringZMReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	pointZMs, err := readLinearRingZM(r, byteOrder)
+	if err != nil {
+		return nil, err
+	}
+	return LineStringZM{pointZMs}, nil
+}
+
+func polygonReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	var numRings uint32
+	if err := binary.Read(r, byteOrder, &numRings); err != nil {
+		return nil, err
+	}
+	rings := make([]LinearRing, numRings)
+	for i := uint32(0); i < numRings; i++ {
+		if points, err := readLinearRing(r, byteOrder); err != nil {
+			return nil, err
+		} else {
+			rings[i] = points
+		}
+	}
+	return Polygon{rings}, nil
+}
+
+func polygonZReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	var numRings uint32
+	if err := binary.Read(r, byteOrder, &numRings); err != nil {
+		return nil, err
+	}
+	ringZs := make([]LinearRingZ, numRings)
+	for i := uint32(0); i < numRings; i++ {
+		if pointZs, err := readLinearRingZ(r, byteOrder); err != nil {
+			return nil, err
+		} else {
+			ringZs[i] = pointZs
+		}
+	}
+	return PolygonZ{ringZs}, nil
+}
+
+func polygonMReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	var numRings uint32
+	if err := binary.Read(r, byteOrder, &numRings); err != nil {
+		return nil, err
+	}
+	ringMs := make([]LinearRingM, numRings)
+	for i := uint32(0); i < numRings; i++ {
+		if pointMs, err := readLinearRingM(r, byteOrder); err != nil {
+			return nil, err
+		} else {
+			ringMs[i] = pointMs
+		}
+	}
+	return PolygonM{ringMs}, nil
+}
+
+func polygonZMReader(r io.Reader, byteOrder binary.ByteOrder) (Geom, error) {
+	var numRings uint32
+	if err := binary.Read(r, byteOrder, &numRings); err != nil {
+		return nil, err
+	}
+	ringZMs := make([]LinearRingZM, numRings)
+	for i := uint32(0); i < numRings; i++ {
+		if pointZMs, err := readLinearRingZM(r, byteOrder); err != nil {
+			return nil, err
+		} else {
+			ringZMs[i] = pointZMs
+		}
+	}
+	return PolygonZM{ringZMs}, nil
+}
+
+func WKBRead(r io.Reader) (Geom, error) {
+
+	var wkbByteOrder uint8
+	if err := binary.Read(r, binary.LittleEndian, &wkbByteOrder); err != nil {
+		return nil, err
+	}
+	var byteOrder binary.ByteOrder
+	switch wkbByteOrder {
+	case wkbXDR:
+		byteOrder = binary.BigEndian
+	case wkbNDR:
+		byteOrder = binary.LittleEndian
+	default:
+		return nil, fmt.Errorf("invalid byte order %u", wkbByteOrder)
+	}
+
+	var wkbGeometryType uint32
+	if err := binary.Read(r, byteOrder, &wkbGeometryType); err != nil {
+		return nil, err
+	}
+
+	if reader, ok := wkbReaders[wkbGeometryType]; ok {
+		return reader(r, byteOrder)
+	} else {
+		return nil, fmt.Errorf("unsupported geometry type %u", wkbGeometryType)
+	}
+
+}
+
+func writeMany(w io.Writer, byteOrder binary.ByteOrder, data ...interface{}) error {
+	for _, datum := range data {
+		if err := binary.Write(w, byteOrder, datum); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writePoint(w io.Writer, byteOrder binary.ByteOrder, point Point) error {
+	return writeMany(w, byteOrder, point.X, point.Y)
+}
+
+func writePointZ(w io.Writer, byteOrder binary.ByteOrder, pointZ PointZ) error {
+	return writeMany(w, byteOrder, pointZ.X, pointZ.Y, pointZ.Z)
+}
+
+func writePointM(w io.Writer, byteOrder binary.ByteOrder, pointM PointM) error {
+	return writeMany(w, byteOrder, pointM.X, pointM.Y, pointM.M)
+}
+
+func writePointZM(w io.Writer, byteOrder binary.ByteOrder, pointZM PointZM) error {
+	return writeMany(w, byteOrder, pointZM.X, pointZM.Y, pointZM.Z, pointZM.M)
+}
+
+func writeLinearRing(w io.Writer, byteOrder binary.ByteOrder, linearRing []Point) error {
+	binary.Write(w, byteOrder, uint32(len(linearRing)))
+	for _, point := range linearRing {
+		if err := writePoint(w, byteOrder, point); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writeLinearRingZ(w io.Writer, byteOrder binary.ByteOrder, linearRingZ []PointZ) error {
+	binary.Write(w, byteOrder, uint32(len(linearRingZ)))
+	for _, pointZ := range linearRingZ {
+		if err := writePointZ(w, byteOrder, pointZ); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writeLinearRingM(w io.Writer, byteOrder binary.ByteOrder, linearRingM []PointM) error {
+	binary.Write(w, byteOrder, uint32(len(linearRingM)))
+	for _, pointM := range linearRingM {
+		if err := writePointM(w, byteOrder, pointM); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func writeLinearRingZM(w io.Writer, byteOrder binary.ByteOrder, linearRingZM []PointZM) error {
+	binary.Write(w, byteOrder, uint32(len(linearRingZM)))
+	for _, pointZM := range linearRingZM {
+		if err := writePointZM(w, byteOrder, pointZM); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (point Point) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	return writePoint(w, byteOrder, point)
+}
+
+func (pointZ PointZ) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	return writePointZ(w, byteOrder, pointZ)
+}
+
+func (pointM PointM) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	return writePointM(w, byteOrder, pointM)
+}
+
+func (pointZM PointZM) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	return writePointZM(w, byteOrder, pointZM)
+}
+
+func (lineString LineString) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	return writeLinearRing(w, byteOrder, lineString.Points)
+}
+
+func (lineStringZ LineStringZ) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	return writeLinearRingZ(w, byteOrder, lineStringZ.Points)
+}
+
+func (lineStringM LineStringM) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	return writeLinearRingM(w, byteOrder, lineStringM.Points)
+}
+
+func (lineStringZM LineStringZM) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	return writeLinearRingZM(w, byteOrder, lineStringZM.Points)
+}
+
+func (polygon Polygon) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	if err := binary.Write(w, byteOrder, uint32(len(polygon.Rings))); err != nil {
+		return err
+	}
+	for _, ring := range polygon.Rings {
+		if err := writeLinearRing(w, byteOrder, ring); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (polygonZ PolygonZ) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	if err := binary.Write(w, byteOrder, uint32(len(polygonZ.Rings))); err != nil {
+		return err
+	}
+	for _, ring := range polygonZ.Rings {
+		if err := writeLinearRingZ(w, byteOrder, ring); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (polygonM PolygonM) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	if err := binary.Write(w, byteOrder, uint32(len(polygonM.Rings))); err != nil {
+		return err
+	}
+	for _, ring := range polygonM.Rings {
+		if err := writeLinearRingM(w, byteOrder, ring); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (polygonZM PolygonZM) wkbWrite(w io.Writer, byteOrder binary.ByteOrder) error {
+	err := binary.Write(w, byteOrder, uint32(len(polygonZM.Rings)))
+	if err != nil {
+		return err
+	}
+	for _, ring := range polygonZM.Rings {
+		if err := writeLinearRingZM(w, byteOrder, ring); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func WKBWrite(w io.Writer, byteOrder binary.ByteOrder, g WKBGeom) error {
+	var wkbByteOrder uint8
+	switch byteOrder {
+	case XDR:
+		wkbByteOrder = wkbXDR
+	case NDR:
+		wkbByteOrder = wkbNDR
+	default:
+		return fmt.Errorf("unsupported byte order %v", byteOrder)
+	}
+	if err := writeMany(w, byteOrder, wkbByteOrder, g.wkbGeometryType()); err != nil {
+		return err
+	}
+	return g.wkbWrite(w, byteOrder)
 }
