02cad02d2100284ff55075761c4be9e0e4d77580
Tom Payne <twpayne@gmail.com>
2013-10-06 17:45:52 +0200

    Factor out linearring

--- src/geom/geom.go
+++ src/geom/geom.go
@@ -4,35 +4,6 @@ type T interface {
 	Bounds() *Bounds
 }
 
-type LinearRing []Point
-
-func (linearRing LinearRing) Bounds() *Bounds {
-	return NewBounds().ExtendLinearRing(linearRing)
-}
-
-type LinearRingZ []PointZ
-
-func (linearRingZ LinearRingZ) Bounds() *Bounds {
-	return NewBounds().ExtendLinearRingZ(linearRingZ)
-}
-
-type LinearRingM []PointM
-
-func (linearRingM LinearRingM) Bounds() *Bounds {
-	return NewBounds().ExtendLinearRingM(linearRingM)
-}
-
-type LinearRingZM []PointZM
-
-func (linearRingZM LinearRingZM) Bounds() *Bounds {
-	return NewBounds().ExtendLinearRingZM(linearRingZM)
-}
-
-type LinearRings []LinearRing
-type LinearRingZs []LinearRingZ
-type LinearRingMs []LinearRingM
-type LinearRingZMs []LinearRingZM
-
 type LineString struct {
 	Points LinearRing
 }
