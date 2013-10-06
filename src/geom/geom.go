6d915c213050cdba536a1d147c439d5ee8a4683a
Tom Payne <twpayne@gmail.com>
2013-10-06 17:48:12 +0200

    Factor out linestring

--- src/geom/geom.go
+++ src/geom/geom.go
@@ -4,38 +4,6 @@ type T interface {
 	Bounds() *Bounds
 }
 
-type LineString struct {
-	Points LinearRing
-}
-
-func (lineString LineString) Bounds() *Bounds {
-	return lineString.Points.Bounds()
-}
-
-type LineStringZ struct {
-	Points LinearRingZ
-}
-
-func (lineStringZ LineStringZ) Bounds() *Bounds {
-	return lineStringZ.Points.Bounds()
-}
-
-type LineStringM struct {
-	Points LinearRingM
-}
-
-func (lineStringM LineStringM) Bounds() *Bounds {
-	return lineStringM.Points.Bounds()
-}
-
-type LineStringZM struct {
-	Points LinearRingZM
-}
-
-func (lineStringZM LineStringZM) Bounds() *Bounds {
-	return lineStringZM.Points.Bounds()
-}
-
 type Polygon struct {
 	Rings LinearRings
 }
