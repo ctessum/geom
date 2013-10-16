e951c4f025b227051a4ddd8858c658cb96dd6915
Tom Payne <twpayne@gmail.com>
2013-10-16 21:25:34 +0200

    Sort values more numerically

--- geom/encoding/wkb/wkb.go
+++ geom/encoding/wkb/wkb.go
@@ -14,44 +14,44 @@ const (
 
 const (
 	wkbPoint                = 1
-	wkbPointM               = 2001
 	wkbPointZ               = 1001
+	wkbPointM               = 2001
 	wkbPointZM              = 3001
 	wkbLineString           = 2
-	wkbLineStringM          = 2002
 	wkbLineStringZ          = 1002
+	wkbLineStringM          = 2002
 	wkbLineStringZM         = 3002
 	wkbPolygon              = 3
-	wkbPolygonM             = 2003
 	wkbPolygonZ             = 1003
+	wkbPolygonM             = 2003
 	wkbPolygonZM            = 3003
 	wkbMultiPoint           = 4
-	wkbMultiPointM          = 2004
 	wkbMultiPointZ          = 1004
+	wkbMultiPointM          = 2004
 	wkbMultiPointZM         = 3004
 	wkbMultiLineString      = 5
-	wkbMultiLineStringM     = 2005
 	wkbMultiLineStringZ     = 1005
+	wkbMultiLineStringM     = 2005
 	wkbMultiLineStringZM    = 3005
 	wkbMultiPolygon         = 6
-	wkbMultiPolygonM        = 2006
 	wkbMultiPolygonZ        = 1006
+	wkbMultiPolygonM        = 2006
 	wkbMultiPolygonZM       = 3006
 	wkbGeometryCollection   = 7
-	wkbGeometryCollectionM  = 2007
 	wkbGeometryCollectionZ  = 1007
+	wkbGeometryCollectionM  = 2007
 	wkbGeometryCollectionZM = 3007
 	wkbPolyhedralSurface    = 15
-	wkbPolyhedralSurfaceM   = 2015
 	wkbPolyhedralSurfaceZ   = 1015
+	wkbPolyhedralSurfaceM   = 2015
 	wkbPolyhedralSurfaceZM  = 3015
 	wkbTIN                  = 16
-	wkbTINM                 = 2016
 	wkbTINZ                 = 1016
+	wkbTINM                 = 2016
 	wkbTINZM                = 3016
 	wkbTriangle             = 17
-	wkbTriangleM            = 2017
 	wkbTriangleZ            = 1017
+	wkbTriangleM            = 2017
 	wkbTriangleZM           = 3017
 )
 
