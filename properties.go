bbec67b0b724ca8241faaaf6c8734fa71861b7a9
Chris Tessum <ctessum@gmail.com>
2014-05-07 12:18:22 -0500

    removed rtree

--- properties.go
+++ properties.go
@@ -284,24 +284,3 @@ func distPointToSegment(p, segStart, segEnd geom.Point) float64 {
 func pointOnSegment(p, segStart, segEnd geom.Point) bool {
 	return distPointToSegment(p, segStart, segEnd) < tolerance
 }
-
-// Calculate the rtreego bounding box of a geom shape.
-func GeomToRect(g geom.T) (bounds *rtreego.Rect, err error) {
-	b := geom.NewBounds()
-	b = g.Bounds(b)
-	p := rtreego.Point{b.Min.X, b.Min.Y}
-	lengths := []float64{b.Max.X - b.Min.X,
-		b.Max.Y - b.Min.Y}
-	if lengths[0] == 0. {
-		lengths[0] = 1.e-6
-	}
-	if lengths[1] == 0. {
-		lengths[1] = 1.e-6
-	}
-	bounds, err = rtreego.NewRect(p, lengths)
-	if err != nil {
-		fmt.Println(p, lengths)
-		return
-	}
-	return
-}
