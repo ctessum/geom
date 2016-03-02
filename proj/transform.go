4f34939831778f4ca81a982a12a0c989a5f860b4
Chris Tessum <ctessum@gmail.com>
2016-03-01 19:08:38 -0600

    proj api change

--- proj/transform.go
+++ proj/transform.go
@@ -1,91 +1,100 @@
 package proj
 
+import "math"
+
 func checkNotWGS(source, dest *SR) bool {
 	return ((source.datum.datum_type == pjd3Param || source.datum.datum_type == pjd7Param) && dest.DatumCode != "WGS84")
 }
 
-// Transform transforms a point from the source to destination spatial reference.
-func Transform(source, dest *SR, point []float64) ([]float64, error) {
-	var wgs84 *SR
-	var err error
-	// Workaround for datum shifts towgs84, if either source or destination projection is not wgs84
-	if checkNotWGS(source, dest) || checkNotWGS(dest, source) {
-		wgs84, err = Parse("WGS84")
-		if err != nil {
-			return nil, err
+// NewTransformFunc creates a function that transforms a point from sr
+// to the destination spatial reference.
+func (source *SR) NewTransformFunc(dest *SR) (TransformFunc, error) {
+	return func(x, y float64) (float64, float64, error) {
+		point := []float64{x, y}
+		var wgs84 *SR
+		var err error
+		// Workaround for datum shifts towgs84, if either source or destination projection is not wgs84
+		if checkNotWGS(source, dest) || checkNotWGS(dest, source) {
+			wgs84, err = Parse("WGS84")
+			if err != nil {
+				return math.NaN(), math.NaN(), err
+			}
+			t, err := source.NewTransformFunc(wgs84)
+			if err != nil {
+				return math.NaN(), math.NaN(), err
+			}
+			point[0], point[1], err = t(point[0], point[1])
+			if err != nil {
+				return math.NaN(), math.NaN(), err
+			}
+			source = wgs84
 		}
-		point, err = Transform(source, wgs84, point)
+		var sourceInverse, destForward TransformFunc
+		_, sourceInverse, err = source.TransformFuncs()
 		if err != nil {
-			return nil, err
+			return math.NaN(), math.NaN(), err
+		}
+		destForward, _, err = source.TransformFuncs()
+		if err != nil {
+			return math.NaN(), math.NaN(), err
 		}
 
-		source = wgs84
-	}
-	var sourceInverse, destForward TransformFunc
-	_, sourceInverse, err = source.TransformFuncs()
-	if err != nil {
-		return nil, err
-	}
-	destForward, _, err = source.TransformFuncs()
-	if err != nil {
-		return nil, err
-	}
-
-	// DGR, 2010/11/12
-	if source.Axis != "enu" {
-		adjust_axis(source, false, point)
-	}
-	// Transform source points to long/lat, if they aren't already.
-	if source.Name == "longlat" {
-		point[0] *= deg2rad // convert degrees to radians
-		point[1] *= deg2rad
-	} else {
-		point[0] *= source.ToMeter
-		point[1] *= source.ToMeter
-		point[0], point[1], err = sourceInverse(point[0], point[1]) // Convert Cartesian to longlat
-		if err != nil {
-			return nil, err
+		// DGR, 2010/11/12
+		if source.Axis != "enu" {
+			adjust_axis(source, false, point)
 		}
-	}
-	// Adjust for the prime meridian if necessary
-	point[0] += source.FromGreenwich
-
-	// Convert datums if needed, and if possible.
-	z := 0.
-	if len(point) == 3 {
-		z = point[2]
-	}
-	point[0], point[1], z, err = datumTransform(source.datum, dest.datum, point[0],
-		point[1], z)
-	if err != nil {
-		return nil, err
-	}
-	if len(point) == 3 {
-		point[2] = 2
-	}
-
-	// Adjust for the prime meridian if necessary
-	point[0] -= dest.FromGreenwich
-
-	if dest.Name == "longlat" {
-		// convert radians to decimal degrees
-		point[0] *= r2d
-		point[0] *= r2d
-	} else { // else project
-		point[0], point[1], err = destForward(point[0], point[1])
-		if err != nil {
-			return nil, err
+		// Transform source points to long/lat, if they aren't already.
+		if source.Name == "longlat" {
+			point[0] *= deg2rad // convert degrees to radians
+			point[1] *= deg2rad
+		} else {
+			point[0] *= source.ToMeter
+			point[1] *= source.ToMeter
+			point[0], point[1], err = sourceInverse(point[0], point[1]) // Convert Cartesian to longlat
+			if err != nil {
+				return math.NaN(), math.NaN(), err
+			}
 		}
-		point[0] /= dest.ToMeter
-		point[1] /= dest.ToMeter
-	}
+		// Adjust for the prime meridian if necessary
+		point[0] += source.FromGreenwich
 
-	// DGR, 2010/11/12
-	if dest.Axis != "enu" {
-		point, err = adjust_axis(dest, true, point)
-		if err != nil {
-			return nil, err
+		// Convert datums if needed, and if possible.
+		z := 0.
+		if len(point) == 3 {
+			z = point[2]
 		}
-	}
-	return point, nil
+		point[0], point[1], z, err = datumTransform(source.datum, dest.datum, point[0],
+			point[1], z)
+		if err != nil {
+			return math.NaN(), math.NaN(), err
+		}
+		if len(point) == 3 {
+			point[2] = 2
+		}
+
+		// Adjust for the prime meridian if necessary
+		point[0] -= dest.FromGreenwich
+
+		if dest.Name == "longlat" {
+			// convert radians to decimal degrees
+			point[0] *= r2d
+			point[0] *= r2d
+		} else { // else project
+			point[0], point[1], err = destForward(point[0], point[1])
+			if err != nil {
+				return math.NaN(), math.NaN(), err
+			}
+			point[0] /= dest.ToMeter
+			point[1] /= dest.ToMeter
+		}
+
+		// DGR, 2010/11/12
+		if dest.Axis != "enu" {
+			point, err = adjust_axis(dest, true, point)
+			if err != nil {
+				return math.NaN(), math.NaN(), err
+			}
+		}
+		return point[0], point[1], nil
+	}, nil
 }
